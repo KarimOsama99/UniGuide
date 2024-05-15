8bce0125102943887b7af99d278ae1e28e003128
KarimOsama99 <ko5418301@gmail.com>
2024-05-15 20:44:57 +0300

    v1.5

--- js/details.js
+++ js/details.js
@@ -2,7 +2,13 @@ $(function () {
     'use strict';
     $(".info-list li").click(function () {
       $(this).addClass("active").siblings().removeClass("active");
-      $('.info-content .accordion-item').hide();
+      $('.info-content .accordion-item, .removes').hide();
       $('.' + $(this).data('class')).fadeIn();
+    });
+
+    $(".info-list2 li").click(function () {
+      $(this).addClass("active").siblings().removeClass("active");
+      $(".info-content2 .remo").hide();
+      $("." + $(this).data("class")).fadeIn();
     });
 });
