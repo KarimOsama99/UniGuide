4d9ce212a8673f5a76fcf54d90228920624e62e8
KarimOsama99 <ko5418301@gmail.com>
2024-05-30 20:50:32 +0300

    js

---
+++ js/filterSide.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+            $(".showFIlter").click(function () {
+                $("body").toggleClass("showFilter");
+              });
+            $(".closeFilter , .close-bg").click(function () {
+              $("body").removeClass("showFilter menuToggled");
+            }),
+              $(document).on("click", ".delete-filter", function (e) {
+                var t = $(this).parent(".the-filter").attr("theval"),
+                  n = $(this).parent(".the-filter").attr("thefiltername");
+                (ve = !0),
+                  $("input[name*='" + n + "'][value*='" + t + "']")
+                    .parent(".theInput")
+                    .click();
+              });
+});
